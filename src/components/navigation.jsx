042bf35339eab4a79a08a99ccc41cd24339a4fa3
Shaish Guni <gunisguni2007@gmail.com>
2021-07-01 00:07:13 +0545

    master

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            Emler.
+            Reachme.
           </a>{' '}
         </div>
 
